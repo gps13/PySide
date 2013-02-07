db79edc2f32f9233ea5c8566b1c1d72649b3cbaa
gps13 <g.psistakis@gmail.com>
2013-02-07 15:46:34 +0200

    Icon+tooltip+button

--- pyside_2.py
+++ pyside_2.py
@@ -1,8 +1,8 @@
 """
 ZetCode PySide tutorial 
 
-This example shows an icon
-in the titlebar of the window.
+This example shows an icon in the titlebar of the window.
+This example shows a tooltip on a window and a button
 
 author: Jan Bodnar
 website: zetcode.com 
@@ -14,17 +14,28 @@ class Example(QtGui.QWidget):	#Example class inherits from QtGui.QWidget class.
     
     def __init__(self):
         super(Example, self).__init__()
-        
         self.initUI()
         
     def initUI(self):	#All three methods have been inherited from the QtGui.QWidget class. 
+        ### tooltip - start ###
+        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10)) #This static method sets a font used to render tooltips.
+        self.setToolTip('This is a <b>QWidget</b> widget') #To create a tooltip, we call the setTooltip() method. We can use rich text formatting.
+
+        btn = QtGui.QPushButton('Button', self)	#We create a button widget
+        btn.setToolTip('This is a <b>QPushButton</b> widget')	#set a tooltip for it.
+        
+        btn.resize(btn.sizeHint()) #The button is being resized and moved on the window.
+        						   #The sizeHint() method gives a recommended size for the button.
+        btn.move(50, 50) 
+        ### tooltip - end ###
+        #########icon - start ###############
         self.setGeometry(300, 300, 250, 150)
         #locates the window on the screen and sets the size of the window.
         #The first two parameters are the x and y positions of the window.
         #The third is the width and the fourth is the height of the window.
         self.setWindowTitle('Icon')
         self.setWindowIcon(QtGui.QIcon('list_icon_world'))        
-    
+    	##########icon - end ##########
         self.show()
 
 
